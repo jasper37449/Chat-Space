6a63797523412da4925ce7551aa0ca67a171367c
Jasper Tan <jasper.tan.luwan@gmail.com>
2025-11-14 17:29:50 +0800

    Installed Axios, Zustand and Initialized them

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
-
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -13,6 +13,11 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
