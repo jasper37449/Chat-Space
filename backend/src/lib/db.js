e8aa453b986ff64a14790f30553898b9051e5345
Jasper Tan <jasper.tan.luwan@gmail.com>
2025-11-18 17:17:22 +0800

    Added dotenv to db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   const uri = process.env.MONGODB_URI; // <-- must match .env key
