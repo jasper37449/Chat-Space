729c32470de46bd715ff098baff281c07bc89e45
Jasper Tan <jasper.tan.luwan@gmail.com>
2025-11-18 15:53:14 +0800

    upadted package.json and git.ignore, ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { server, app } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
@@ -22,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    }
+);}
 
 server.listen(PORT, () => {
     console.log("Server is running on port", PORT);
