139f17f629da9e55fceacc671f6420f11ecaddf5
Jasper Tan <jasper.tan.luwan@gmail.com>
2025-11-18 17:20:49 +0800

    Revert previous change

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const connectDB = async () => {
   const uri = process.env.MONGODB_URI; // <-- must match .env key
