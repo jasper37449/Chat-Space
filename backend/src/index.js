508808feb58eba979cd84cfc172f2e3b2ddefb94
Jasper Tan <jasper.tan.luwan@gmail.com>
2025-11-18 17:09:02 +0800

    Update index.js to match railway's requirements

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,12 +26,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-    }
-);}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+//     }
+// );}
 
 server.listen(PORT, () => {
     console.log("Server is running on port", PORT);
